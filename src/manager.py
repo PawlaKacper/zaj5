c59d3eae4516a5c5ff9a88d032c0757996be12a0
Dominik Klyszynski <dominiklyszynski@gmail.com>
2026-03-31 12:55:24 +0200

    Added method to Manager.py and integration tests

--- src/manager.py
+++ src/manager.py
@@ -23,3 +23,11 @@ class Manager:
             if tenant.apartment not in self.apartments:
                 return False
         return True
+    
+    def get_apartment_costs(self,apartment_key, year, month):
+        if apartment_key not in self.apartments:
+            return 0.0
+        
+        total  = sum(bill.amount_pln for bill in self.bills
+                     if bill.apartment == apartment_key and bill.settlement_year == year and bill.settlement_month == month)
+        return total
--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,6 +1,7 @@
 from src.models import Apartment
 from src.manager import Manager
 from src.models import Parameters
+from src.models import Bill
 
 
 def test_load_data():
@@ -30,3 +31,19 @@ def test_if_tenants_have_valid_apartment_keys():
 
     manager.tenants['tenant-1'].apartment = 'invalid-key'
     assert manager.check_tenants_apartment_keys() == False
+
+def test_get_apartment_costs():
+    parameters = Parameters()
+    manager = Manager(parameters)
+
+    manager.bills = [Bill(apartment='A1',amount_pln=150,date_due='2024-03-05',settlement_year=2024,settlement_month=3, type='woda'),
+                     Bill(apartment='A1',amount_pln=132,date_due='2024-03-11',settlement_year=2024,settlement_month=3, type='prad'),
+                     Bill(apartment='A2',amount_pln=1,date_due='2024-03-12',settlement_year=2024,settlement_month=3, type='woda'),
+                     Bill(apartment='A2',amount_pln=1000,date_due='2024-03-13',settlement_year=2024,settlement_month=3, type='gaz')]
+    
+    manager.apartments = {'A1': Apartment(key='A1', name='Apartment 1',location='Chartowo', area_m2=50,rooms={}),
+                          'A2': Apartment(key='A2', name='Apartment 2',location='Siemianowiceslaskie', area_m2=50,rooms={})}
+
+    assert manager.get_apartment_costs('A532',2024,3) == 0
+    assert manager.get_apartment_costs('A1',2024,3) == 282
+    assert manager.get_apartment_costs('A1',2024,4) == 0
